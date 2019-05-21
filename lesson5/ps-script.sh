08204b15b0924e29200a6685c9cf3ffe427f5b0b
Andrey Agafonov <aagafonov@inbox.ru>
2019-05-22 01:11:21 +0300

    qwet

--- lesson5/ps-script.sh
+++ lesson5/ps-script.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-pidlst=$(ls /proc/ |grep ^[0-9])
-#echo $pidlst
-for i in $pidlst
+#Получаем список PID всех запущенных процессов
+#pids=$(ls /proc/ |grep ^[0-9])
+#сделать по пидам
+for i in $(ls /proc/ |grep ^[0-9])
 do
-awk '{print $1 $2 }' /proc/$i/stat
+
+#pid[$k]=$(awk '{print $7}' /proc/$i/stat)
+#proc[]
+#ttylst[$i]=$(awk '{print $7}' /proc/$i/stat)
+#pidlst[$i]=$(awk '{print $1}' /proc/$i/stat)
+#statlst=
+#timeslt=
+#cmdlst=
+#printf ($pidlst\t,$ttylst\n)
+printf "   %s\t" $(awk '{print $1}' /proc/$i/stat)
+printf "   %s\t" $(awk '{print $7}' /proc/$i/stat)
+printf "   %s\t" $(awk '{print $22}' /proc/$i/stat)
+printf "   %s\t" $(awk '{print $3}' /proc/$i/stat)
+printf "   %s\n" $(awk '{print $2}' /proc/$i/stat)
+
+
+#printf "   %d\t" $index
+#printf "   %d\n" $index
 done
