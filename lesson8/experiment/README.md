ca825117299dddefc1b871c5a475956c199a1a1d
Andrey Agafonov <a.aagafonov@smartcat.pro>
2019-07-09 18:45:20 +0300

    Урок 8 Загрузка системы

--- lesson8/experiment/README.md
+++
@@ -1,6 +0,0 @@
-Задание:
-
-Сконфигурировать систему с /boot на LVM.
-
-
-Решение приведено в виде [Vagrantfile](Vagrantfile) с provision. Ход описан внутри [файла](provision.sh).
--- lesson8/task3/testmodule.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
-echo "----------------------"
-echo "- TEST DRACUT MODULE -"
-echo "----------------------"
-sleep 10
-
